e46bab7ff0b8a08297960e91f4bccac2c70447a5
admin <huweimin@shchuwa.com>
2020-07-07 13:55:03 +0800

    主题添加

--- src/app/COVID19/covid19.component.ts
+++ src/app/COVID19/covid19.component.ts
@@ -2,11 +2,14 @@ import { OnInit, Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BarOption } from '../OptionCreator/BarOption';
 import { LineOption } from '../OptionCreator/LineOption';
+import { Axis } from '../OptionCreator/OptionBase';
+import { ChartComponent } from '../Chart/chart.component';
 @Component({
     templateUrl: './covid19.component.html'
 })
 export class Covid19_Component implements OnInit {
     title = '新冠肺炎2019';
+    chartComp = ChartComponent;
     constructor(private http: HttpClient) {
 
     }
@@ -19,12 +22,21 @@ export class Covid19_Component implements OnInit {
                 this.Line_Total = LineOption.CreateLine(r.map(x => x.PublishDate.substring(0, 10)), r.map(x => x.Confirmed_Total))
                 this.Line_Total.series.push(LineOption.CreateLineItem(r.map(x=>x.Recoved_Total)))
                 this.Line_Total.series.push(LineOption.CreateLineItem(r.map(x=>x.Death_Total)));
+                //双坐标轴
+                let yAxis1 = new Axis();
+                let yAxis2 = new Axis();
+                this.Line_Total.yAxis = [yAxis1,yAxis2]
                 this.Line_Total.series[0].itemStyle.color = "red";
                 this.Line_Total.series[0].name = "累积确诊人数";
+                this.Line_Total.series[0].yAxisIndex = 0;
+
                 this.Line_Total.series[1].itemStyle.color = "green";
                 this.Line_Total.series[1].name = "累积治疗人数";
+                this.Line_Total.series[1].yAxisIndex = 0;
+
                 this.Line_Total.series[2].itemStyle.color = "black";
                 this.Line_Total.series[2].name = "累积死亡人数";
+                this.Line_Total.series[2].yAxisIndex = 1;
             }
         )
     }
